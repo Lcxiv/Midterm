d86977d52c6608196bd8c6d683887f7ecaa07391
Louis Condevaux <condevaux@chapman.edu>
2020-11-01 18:59:46 -0800

    Created a script to format my files, it is in the stats folder rename.sh, run sh rename.sh to rename all the csv files to the same format based on the rules that I created.

---
+++ stats/rename.sh
@@ -0,0 +1,7 @@
+#!bin/bash
+#add -n if you just want to print the result without changing it
+#renaming all my files in a better format with rename(perl)
+
+
+rename -f 's/Challenge//g; s/Stats//g; s/\s+/_/g; s/[^-]-//; s/[^-]-//; s/-[^-]*\_//' *.csv
+
