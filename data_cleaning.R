c9332fa5fbadd5241435bf73411392f46637c712
Louis Condevaux <condevaux@chapman.edu>
2020-11-07 23:07:48 -0800

    Made a graph that wasn't as bad as the previous one, still cannot find a better way to automate the process based on the name of the file

--- data_cleaning.R
+++ data_cleaning.R
@@ -8,12 +8,9 @@ library(lubridate)
 library(sqldf)
 library(magrittr)
 
-setwd("C:/Users/Master/Desktop/510/Midterm")
-
 #Finding Directory containing the data
 
 data_dir <- "stats"   #setting up the directory containing the csv files
-#fs::dir_ls(data_dir)
 
 #Merging csv files by scenarios 
 
@@ -27,22 +24,82 @@ names(one_wall_5)[2] <- "Kills"
 
 #Adding new column based on date of the scenario
 
-x <- one_wall_5$source
-x %<>%
+rename_one_wall_5 <- one_wall_5$source
+rename_one_wall_5 %<>%
   gsub("^(.{25})","",.) %>%
   gsub(".csv","",.) %>%
   gsub("\\.","-",.)
-x
-one_wall_5$date <- x
+
+one_wall_5$date <- rename_one_wall_5
+one_wall_5$date<- as.Date(one_wall_5$date)
 
 
 #Creating new data frame with columns of interest
 
 sql_one_wall5 <- sqldf('SELECT  Kills, Bot, Timestamp, date FROM one_wall_5 
-           WHERE Kills == "pistol" GROUP BY date')
+           WHERE Kills == "pistol"')
 sql_one_wall5$Bot <- as.double(sql_one_wall5$Bot)
 sql_one_wall5$Timestamp <- as.double(sql_one_wall5$Timestamp)
 
-#Measuring Accuracy and creating a new column
+#Measuring Accuracy and creating a new column, only using accuracy right now
 
 sql_one_wall5$accuracy <- sql_one_wall5$Bot/sql_one_wall5$Timestamp
+
+
+
+#Plotting graph
+
+p <- ggplot(sql_one_wall5, aes(x=date, y=accuracy)) +
+  geom_line( color="steelblue") + 
+  geom_point() +
+  xlab("") +
+  theme(axis.text.x=element_text(angle=60, hjust=1)) +
+  scale_x_date(limit=c(as.Date("2020-07-29"),as.Date("2020-10-18"))) +
+  ylim(0.5,1)
+p
+
+
+#Repeating process for a different Scenario
+
+Bounce_180 <- data_dir %>% 
+  dir_ls(regexp = "Bounce_180_*") %>% 
+  map_dfr(read_csv, .id = "source")
+
+names(Bounce_180)[2] <- "Kills"
+
+#Adding new column based on date of the scenario
+
+Bounce_rename <- Bounce_180$source
+Bounce_rename %<>%
+  gsub("^(.{17})","",.) %>%
+  gsub(".csv","",.) %>%
+  gsub("\\.","-",.)
+
+Bounce_180$date <- Bounce_rename
+Bounce_180$date<- as.Date(Bounce_180$date)
+s <- as.Date(Bounce_rename)
+
+
+#Creating new data frame with columns of interest
+
+sql_bounce_180 <- sqldf('SELECT  Kills, Bot, Timestamp, date FROM Bounce_180 
+           WHERE Kills == "pistol"')
+sql_bounce_180$Bot <- as.double(sql_bounce_180$Bot)
+sql_bounce_180$Timestamp <- as.double(sql_bounce_180$Timestamp)
+
+#Measuring Accuracy and creating a new column
+
+sql_bounce_180$accuracy <- sql_bounce_180$Bot/sql_bounce_180$Timestamp
+
+
+
+#Plotting graph
+
+p <- ggplot(sql_bounce_180, aes(x=date, y=accuracy)) +
+  geom_line( color="steelblue") + 
+  geom_point() +
+  xlab("") +
+  theme(axis.text.x=element_text(angle=60, hjust=1)) +
+  scale_x_date(limit=c(as.Date("2020-07-08"),as.Date("2020-10-18"))) +
+  ylim(0.5,1)
+p
